9e9a1ad13cde0069b70065180e1389d33ae180ad
pc-portable <martin.stephan9218@gmail.com>
2025-04-15 12:10:23 +0200

    exo 3.2 partie 3e & 4e partie

--- sql/3.2.sql
+++ sql/3.2.sql
@@ -1,25 +1,45 @@
 /* Trouver tout les voyages et leurs informations */
 SELECT travel_id, total_price, country, city, accom_name, accom_category, limit_rooms, comp_name, veh_type, veh_name, limit_seats
-    FROM reservation
-INNER JOIN travelagency
-    ON reservation.travel = travelagency.travel_id
-INNER JOIN payment
-    ON reservation.payment = payment.pay_id
-INNER JOIN destination
-    ON travelagency.destination = destination.destination_id
-INNER JOIN accomodation
-    ON destination.accomodation = accomodation.accom_id
-INNER JOIN company
-    ON travelagency.transport = company.comp_id
-INNER JOIN vehicle
-    ON company.transport_mode = vehicle.veh_id
+    FROM reservation res
+INNER JOIN travelagency tra
+    ON res.travel = tra.travel_id
+INNER JOIN payment pay
+    ON res.payment = pay.pay_id
+INNER JOIN destination dest
+    ON tra.destination = dest.destination_id
+INNER JOIN accomodation acc
+    ON dest.accomodation = acc.accom_id
+INNER JOIN company comp
+    ON tra.transport = comp.comp_id
+INNER JOIN vehicle veh
+    ON comp.transport_mode = veh.veh_id
 ;
 
 /* Trouver la reservation d'un client et le statut de paiement */
-SELECT res_id, cl_firstname, cl_lastname, res_date, pay_type, pay_date, total_price, pay_status FROM reservation
-INNER JOIN payment
-	ON  reservation.payment = payment.pay_id
-INNER JOIN `client`
-	ON reservation.`client` = `client`.cl_id
+SELECT res_id, cl_firstname, cl_lastname, res_date, pay_type, pay_date, total_price, pay_status
+    FROM reservation res
+INNER JOIN payment pay
+	ON  res.payment = pay.pay_id
+INNER JOIN `client` cli
+	ON res.`client` = cli.cl_id
 WHERE res_id = '2'
+;
+
+/* Trouver les transports disponibles pour un voyage donné */
+SELECT travel_id, country, city, comp_name, veh_type, veh_name, limit_seats, transport_price
+    FROM travelagency tra
+INNER JOIN destination dest
+	ON tra.destination = dest.destination_id
+INNER JOIN company comp
+	ON tra.transport = comp.comp_id
+INNER JOIN vehicle veh
+	ON comp.transport_mode = veh.veh_id
+WHERE travel_id = '1'
+;
+
+/* Consulter les informations d’une entreprise et ses services associés. */
+SELECT comp_name, veh_type, veh_name, limit_seats, transport_price FROM company
+INNER JOIN vehicle
+	ON company.transport_mode = vehicle.veh_id
+WHERE comp_id = '4'
 ;
