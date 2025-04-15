92678e13992f393bc8a8998f86698bc8c8e15d0d
pc-portable <martin.stephan9218@gmail.com>
2025-04-15 11:54:13 +0200

    exo 3.2 partie 2e partie

--- sql/3.2.sql
+++ sql/3.2.sql
@@ -1,3 +1,4 @@
+/* Trouver tout les voyages et leurs informations */
 SELECT travel_id, total_price, country, city, accom_name, accom_category, limit_rooms, comp_name, veh_type, veh_name, limit_seats
     FROM reservation
 INNER JOIN travelagency
@@ -12,4 +13,13 @@ INNER JOIN company
     ON travelagency.transport = company.comp_id
 INNER JOIN vehicle
     ON company.transport_mode = vehicle.veh_id
+;
+
+/* Trouver la reservation d'un client et le statut de paiement */
+SELECT res_id, cl_firstname, cl_lastname, res_date, pay_type, pay_date, total_price, pay_status FROM reservation
+INNER JOIN payment
+	ON  reservation.payment = payment.pay_id
+INNER JOIN `client`
+	ON reservation.`client` = `client`.cl_id
+WHERE res_id = '2'
 ;
